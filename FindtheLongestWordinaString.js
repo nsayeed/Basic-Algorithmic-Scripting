ac505f39fac032bad93e15a14817bb8d2ba827aa
Nabil <nsayeed@users.noreply.github.com>
2016-10-17 21:57:03 +0100

    Update FindtheLongestWordinaString.js

--- FindtheLongestWordinaString.js
+++ FindtheLongestWordinaString.js
@@ -4,9 +4,9 @@ function findLongestWord(str) {
   var word = sstring[0]; //assign the first word in the array to the var 'word'
   var i = 0; //set var i to 0
   
-  for(i=0; i < sstring.length; i++){
-    if(sstring[i].length > word.length){
-      word = sstring[i];
+  for(i=0; i < sstring.length; i++){    //As i increased the string is checked for the length of each word. 
+    if(sstring[i].length > word.length){ //Length of new word checked against previous
+      word = sstring[i];  //if the length of the new word is longer than the old then the new word is assigned to the var 'word'
     }
   }
   
